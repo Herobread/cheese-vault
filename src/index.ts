3a86fd9f1dc67e99044962f047d7cfd0de55abe1
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-06-12 15:34:23 +0100

    feat: simple echo bot

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-console.log("Hello, cheease!")
-const a: number = 12
+import "dotenv/config"
+import TelegramBot from "node-telegram-bot-api"
+
+const token = process.env.TELEGRAM_API_KEY || ""
+
+console.log(token)
+
+const bot = new TelegramBot(token, { polling: true })
+
+bot.onText(
+    /\/echo (.+)/,
+    (msg: TelegramBot.Message, match: RegExpExecArray | null) => {
+        if (!match) return
+
+        const chatId = msg.chat.id
+        const resp = match[1]
+
+        bot.sendMessage(chatId, resp)
+    }
+)
