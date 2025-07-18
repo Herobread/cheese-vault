bda1d1e1237a989756065ab64b8094d3a3862b5f
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-07-18 20:28:04 +0100

    refactor: update .gitignore to include coverage directory and clean up connection.ts

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,10 +1,4 @@
 import "dotenv/config"
-import { sql, SQL } from "drizzle-orm"
 import { drizzle } from "drizzle-orm/libsql"
-import { AnySQLiteColumn } from "drizzle-orm/sqlite-core"
 
 export const db = drizzle(process.env.DB_FILE_NAME!)
-
-export function lower(email: AnySQLiteColumn): SQL {
-    return sql`(lower(${email}))`
-}
