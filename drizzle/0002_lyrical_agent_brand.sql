770319f11dbc05fcccfb377ab4831792ad8c50ac
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-07-18 14:08:18 +0100

    feat: add unique constraints to shopping_items and shopping_lists tables, enhance test coverage for getMainListId

---
+++ drizzle/0002_lyrical_agent_brand.sql
@@ -0,0 +1,2 @@
+CREATE UNIQUE INDEX `shopping_items_item_id_unique` ON `shopping_items` (`item_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `shopping_lists_list_id_unique` ON `shopping_lists` (`list_id`);
