2cd4839bfd602044945c83bd4c8494e99e79d013
Oleksii Nazarenko <alexeybread81@gmail.com>
2025-07-26 12:41:35 +0100

    feat: add help command to provide users with command usage instructions

---
+++ src/commands/helpCommand.ts
@@ -0,0 +1,28 @@
+import { Context } from "telegraf"
+import { Message, Update } from "telegraf/typings/core/types/typegram"
+
+export async function helpCommandHandler(
+    ctx: Context<Update.MessageUpdate<Message.TextMessage>>
+) {
+    const helpMessage = `*🧀 Welcome to Cheese Vault Bot\\!*
+
+Here are the commands you can use:
+
+*🛒 Item Management*
+\`/add\` \`<item name>\`
+you can also add list name before item name to add it to specific list
+\`/add\` \`<list name>\` \`<item name>\`
+
+\`/delete\` \`<item id>\` or \`/del\` \`<item id>\` \\- Delete an item
+
+*🗒️ List Management*
+\`/list\` \\- show all items
+\`/lists\` \\- show lists
+\`/addList\` \`<list name>\` \\- create a list \\(no spaces\\!\\)
+\`/deleteList\` \`<list name>\` \\- delete a shopping list and all its items
+    
+*💡 Pro Tip*
+You can also add items by simply replying to the pinned list message with the item name
+`
+    ctx.sendMessage(helpMessage, { parse_mode: "MarkdownV2" })
+}
